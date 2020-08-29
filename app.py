bdd5d61fe3e22589d8aac03e0e36b2a9c9b4aece
swarnadipmukherjeeofficial1999 <70023230+swarnadipmukherjeeofficial1999@users.noreply.github.com>
2020-08-29 11:05:15 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,29 @@
+from flask import Flask,render_template,url_for,request
+import pandas as pd 
+import pickle
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.externals import joblib
+import pickle
+
+
+filename ='pickle.pkl'
+clf = pickle.load(open(filename,'rb'))
+vectorizer=pickle.load(open('tranformer.pkl','rb'))
+
+app = Flask(__name__, template_folder='template', static_folder='static')
+@app.route('/')
+def home():
+	return render_template('home.html')
+
+@app.route('/predict',methods=['POST'])
+def predict():
+	if request.method == 'POST':
+		message = request.form['message']
+		data = [message]
+		vect = vectorizer.transform(data).toarray()
+		my_prediction = clf.predict(vect)
+	return render_template('result.html',prediction = my_prediction)
+
+if __name__ == '__main__':
+	app.run(debug=True)
